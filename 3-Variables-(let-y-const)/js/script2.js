c6264332952b308785974bbfcec0e199d99e4bb7
XOXEMY\juanc <juancarlosrodriguezlopez.jcr42@gmail.com>
2023-11-23 13:21:28 +0100

    3-Variables-(let-y-const)

---
+++ 3-Variables-(let-y-const)/js/script2.js
@@ -0,0 +1,10 @@
+//NO SE PUEDEN INCIAR SIN UN VALOR
+//SE USAN PARA LAS VARIABLES QUE NO VAN A CAMBIAR SU VALOR
+
+const disponible = 'coche'; //Iniciar variables y asignarle valor
+
+const nodisponible = true; //Iniciamos variable sin valor
+
+const producto1 = 'moto',
+    producto2 = true,
+    producto3 = 'camion';
