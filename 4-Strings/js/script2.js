3c484aea6b215ef44f7c525986906ecc904c9df6
XOXEMY\juanc <juancarlosrodriguezlopez.jcr42@gmail.com>
2023-11-23 13:44:39 +0100

    4-Strings

---
+++ 4-Strings/js/script2.js
@@ -0,0 +1,13 @@
+const producto = "Coche 4 puertas con 4 ruedas";
+
+//lenght cuenta el numero de caracteres
+console.log(producto.length);
+
+//IdexOf (devuelve posición)
+
+console.log(producto.indexOf('Coche'));
+
+//Includes (devuelve true o false si encientra la palabra)
+
+console.log(producto.includes('con'));
+
