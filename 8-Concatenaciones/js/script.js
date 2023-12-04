f3184189ad86db22eae20c8db9c2568f18cfe8aa
XOXEMY\juanc <juancarlosrodriguezlopez.jcr42@gmail.com>
2023-12-04 16:33:33 +0100

    8-Concatenaciones

---
+++ 8-Concatenaciones/js/script.js
@@ -0,0 +1,8 @@
+const nombre = "Antonio";
+const email = "antonio@pruebas.com"
+
+//Concatenación
+console.log("Nombre del cliente: " + nombre + " Email: " + email);
+
+//Template String
+console.log(`Nombre del cliente  ${nombre}  Email:  ${email}`);
