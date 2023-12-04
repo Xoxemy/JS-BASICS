72124ffecebc2df727854ad8d7f302ecc8de6017
XOXEMY\juanc <juancarlosrodriguezlopez.jcr42@gmail.com>
2023-12-04 16:09:18 +0100

    7-OrdenesDeOperaciones

---
+++ 7-OrdenesDeOperaciones/js/script.js
@@ -0,0 +1,17 @@
+let resultado;
+
+//Ordenes de Operaciones
+resultado = 20 + 30 * 2;
+
+//Porcentajes
+resultado = (100 + 200 + 300) * 1.16; //Esto te da el 16% de la suma
+
+//Incrementos
+let = incrementos = 10;
+incrementos++;
+console.log(puntuacion)
+
+//Decrenentos
+let = decrementos = 10;
+decrementos--;
+console.log(puntuacion)
